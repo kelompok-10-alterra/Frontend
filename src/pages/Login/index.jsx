ed42bd1aa832e36ff907ed49332b5b77eaa0d012
tatasulysta <tatasulystaaa@gmail.com>
2022-06-17 18:42:50 +0700

    fix width on information components

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -9,49 +9,60 @@ import Form from "../../components/Form";
 import TitleLogo from "../../components/TitleLogo";
 
 const Login = () => {
+  const [inputs, setInputs] = useState([
+    {
+      label: "Username",
+      name: "username",
+      type: "text",
+      placeholder: "Type your username...",
+      value: "",
+    },
+    {
+      label: "Password",
+      name: "password",
+      type: "password",
+      placeholder: "Type your password...",
+      value: "",
+    },
+  ]);
 
-    const [inputs, setInputs] = useState([
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(
+        "/auth/login",
+        JSON.stringify({
+          username: inputs[0].value,
+          password: inputs[1].value,
+        }),
         {
-            label: "Username",
-            name: "username",
-            type: "text",
-            placeholder: "Type your username...",
-            value: "",
-        },
-        {
-            label: "Password",
-            name: "password",
-            type: "password",
-            placeholder: "Type your password...",
-            value: "",
-        },
-    ]);
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      )
+      .then((result) => {
+        console.log(result.data);
+        // localStorage.setItem("token", result.data.token);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    setInputs([...inputs], (inputs[0].value = ""), (inputs[1].value = ""));
+  };
 
-        axios.post("/auth/login", {
-            username: inputs[0].value,
-            password: inputs[1].value
-        }).then(result => {
-            console.log(result.data);
-            // localStorage.setItem("token", result.data.token);
-        }).catch(error => {
-            console.log(error);
-        })
-
-        setInputs([...inputs], inputs[0].value = "", inputs[1].value = "");
-    }
-
-    return (
-        <div className={styles.container}>
-            <form onSubmit={handleSubmit} className={styles.form_container}>
-                <TitleLogo />
-                <Form inputs={inputs} setInputs={setInputs} />
-                <button type="submit" className={styles.login_btn}>Login</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form_container}>
+        <TitleLogo />
+        <Form inputs={inputs} setInputs={setInputs} />
+        <button type="submit" className={styles.login_btn}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
