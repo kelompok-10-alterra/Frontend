220525149872cc0d1cf4f1f7d0ac5bb7fc582cd2
MalvinoTan <malvinoaustintanura@gmail.com>
2022-06-23 09:55:53 +0700

    Beutify code on Newsletter

--- src/pages/NewsLetter/index.jsx
+++ src/pages/NewsLetter/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 /** Component */
 import Button from "../../components/Button";
@@ -30,23 +30,24 @@ const Newsletter = () => {
       value: "",
     },
   ]);
+
   const [lists, setLists] = useState([
     {
       id: 1,
       title: "About Your Privacy",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, earum expedita doloribus, nisi eius id placeat delectus vitae corporis a reprehenderit beatae accusamus. Saepe excepturi velit autem ab exercitationem ea.",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, earum expedita doloribus, nisi eius id placeat delectus vitae corporis a reprehenderit beatae accusamus. Saepe excepturi velit autem ab exercitationem ea.",
     },
     {
       id: 2,
       title: "About Your Privacy",
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, earum expedita doloribus, nisi eius id placeat delectus vitae corporis a reprehenderit beatae accusamus. Saepe excepturi velit autem ab exercitationem ea.",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, earum expedita doloribus, nisi eius id placeat delectus vitae corporis a reprehenderit beatae accusamus. Saepe excepturi velit autem ab exercitationem ea.",
     },
   ]);
+
   const handleClick = () => {
     //
   };
+
   return (
     <>
       <PageTitle icon={<TiNews />} title="Newsletter" />
