85a418d0c48a8e94bd804d7b3f209e82b11ef279
MalvinoTan <malvinoaustintanura@gmail.com>
2022-06-29 20:12:10 +0700

    Make header data dynamic

--- src/components/Layouts/MainLayout/index.jsx
+++ src/components/Layouts/MainLayout/index.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 /** Components */
 import Header from "../../Header";
 
 const MainLayout = () => {
+
+  const user = JSON.parse(localStorage.getItem("token")).user;
+
+  const [role, setRole] = useState("");
+
+  useEffect(() => {
+    user.roles?.forEach(item => {
+      console.log(item)
+      if (item === "ROLE_SUPER_ADMIN") {
+        setRole("Super Admin");
+      }
+      else if (item === "ROLE_ADMIN") {
+        setRole("Admin");
+      }
+    });
+  }, []);
+
   return (
     <>
-      <Header name="Kevin C" role="Super Admin" />
+      <Header name={user.sub} role={role} />
       <Outlet />
     </>
   );
